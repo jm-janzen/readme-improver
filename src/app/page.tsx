ab1ecfa4c4a95ee7d4f9a93de4aad47c75c7beb1
JM <jmjanzen@gmail.com>
2024-12-09 14:06:13 -0500

    fix: update very basic copy
    Design starting to come together.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,17 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>demo</h1>
-      <Link href="#">Here's a link</Link>
+      <h1>README Header Adder</h1>
+
+
+      <div>
+        <label>GitHub repo URL</label>&nbsp;
+        <input placeholder='ex: https://github.com/octocat/Hello-World'></input>
+        <hr />
+        <button>Submit</button>
+      </div>
+
+      <Link href="#">Project Source</Link>
     </div>
   );
 }
