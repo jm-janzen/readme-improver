42440757da260f9f563fc85c796bd304264f62a1
JM <jmjanzen@gmail.com>
2024-12-10 15:17:36 -0500

    fix: stub in strategies
    These will be used to grab repos and
    do something with them. Probably just
    GitHub if I'm being honest with myself...

--- src/app/api/submit-git-url/route.ts
+++ src/app/api/submit-git-url/route.ts
@@ -1,13 +1,47 @@
 import { NextRequest, NextResponse as res } from 'next/server';
+import GitUrlParse from 'git-url-parse'
 
 export async function POST(req: NextRequest) {
     const data = await req.json()
 
     // TODO Actually fetch repo and do something
+    const parsedUrl = GitUrlParse(data.gitUrl)
+    const strategy = repoStrategies[parsedUrl.resource]
+    if (!strategy) {
+        throw new Error(`Unsupported git resource '${parsedUrl.resource}'`)
+    }
+
+    const result = execStrategy(strategy, 'pull', data.gitUrl)
+    console.debug(result)
 
     return res.json({
         gitUrl: data.gitUrl,
+        gitUrlParsed: parsedUrl,
         success: true,
         repo: {},
     })
+}
+
+type Strategy = (op: string, url: string) => {
+    success: boolean,
+    message: string,
+    result: object,
+}
+
+const githubStrategy: Strategy = (data) => {
+    const result = {}
+
+    return {
+        success: true,
+        message: `ran github strategy on ${data}`,
+        result,
+    }
+}
+
+const repoStrategies: { [key: string]: Strategy } = {
+    'github.com': githubStrategy,
+}
+
+const execStrategy = (strategy: Strategy, op: string, url: string) => {
+    return strategy(op, url)
 }
