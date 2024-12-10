d7fe45ac3e8ce2801199c56efd0c18fc8a1969bd
JM <jmjanzen@gmail.com>
2024-12-10 13:04:48 -0500

    fix: init basic submit flow
    Eventually would like to split off from here
    depending on what kind of URL is provided.

---
+++ src/app/api/submit-git-url/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse as res } from 'next/server';
+
+export async function POST(req: NextRequest) {
+    const data = await req.json()
+
+    // TODO Actually fetch repo and do something
+
+    return res.json({
+        gitUrl: data.gitUrl,
+        success: true,
+        repo: {},
+    });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,40 @@
+'use client'
+
+import { FormEvent } from 'react'
 import Link from 'next/link'
 
+
 export default function Home() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>README Header Adder</h1>
+    async function onSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
 
+        const formData = new FormData(event.currentTarget)
+        const body = JSON.stringify({ gitUrl: formData.get('git-url') })
+        const response = await fetch('/api/submit-git-url', {
+          method: 'POST',
+          body,
+        })
+        const data = await response.json()
 
-      <div>
-        <label>GitHub repo URL</label>&nbsp;
-        <input placeholder="ex: https://github.com/octocat/Hello-World"></input>
-        <hr />
-        <button>Submit</button>
-      </div>
+        console.log(`Attempted to look up the git URL ${data.gitUrl} (success: ${data.success})`)
 
-      <Link href="#">Project Source</Link>
-    </div>
-  );
+        // Now how do I translate this into action...
+        return { todo: true }
+    }
+
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>README Header Adder</h1>
+
+            <div>
+                <form onSubmit={onSubmit}>
+                    <label>GitHub repo URL</label>&nbsp;
+                    <input name="git-url" placeholder="ex: https://github.com/octocat/Hello-World" />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+
+            <Link href="#">Project Source</Link>
+        </div>
+    );
 }
