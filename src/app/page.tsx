7fa4f2fb1777367dea317774eaa39ce7e52d53d5
JM <jmjanzen@gmail.com>
2024-12-11 16:45:46 -0500

    refactor: the code should quack too
    Some small UI improvements while here.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ export default function App() {
         const { url, token } = formData
         try {
             const body = JSON.stringify({ url, token })
-            const response = await fetch('/api/submit-git-url', {
+            const response = await fetch('/api/quack', {
                 method: 'POST',
                 body,
             })
@@ -98,23 +98,31 @@ export default function App() {
                                 <Input isRequired label="Git URL" name="git-url" placeholder="Ex: https://github.com/jm-duck/quack-public" type="text" />
                                 <div className="flex gap-2 justify-end">
                                     <Button fullWidth color="primary" type="submit">
-                                        Quack...?
+                                        Parse URL
                                     </Button>
                                 </div>
                             </form>
                         </Tab>
                         <Tab key="auth" title="Authenticate">
-                            <form onSubmit={onSubmitToken} className="flex flex-col gap-4 h-[300px]">
+                            <form onSubmit={onSubmitToken} className="flex flex-col gap-4">
                                 <Input isRequired label="Token" name="github-pat" placeholder="Ex: github_pat_XYZ" type="text" />
+                                <p className="text-center text-small">
+                                    Don't have an access token?{" "}
+                                    <Link size="sm" target="_blank" href="https://github.com/settings/personal-access-tokens/new">
+                                        Get one here
+                                    </Link>
+                                </p>
                                 <div className="flex gap-2 justify-end">
-                                    <Button fullWidth color="primary" type="submit">
-                                        Authenticate Token
-                                    </Button>
+                                    <Tooltip color="warning" delay={500} placement="bottom" content="Requires scope 'Read and Write access to code'">
+                                        <Button fullWidth color="primary" type="submit">
+                                            Authenticate Token
+                                        </Button>
+                                    </Tooltip>
                                 </div>
                             </form>
                         </Tab>
                         <Tab key="quack" title="Quack">
-                            <form onSubmit={onSubmitQuack} className="flex flex-col gap-4 h-[300px]">
+                            <form onSubmit={onSubmitQuack} className="flex flex-col gap-4 h-[50px]">
                                 <div className="flex gap-2 justify-end">
                                     <Button fullWidth color="primary" type="submit">
                                         Quack!
