c38d63f052692a3800c6dc08fd56197da23f6480
JM <jmjanzen@gmail.com>
2024-12-12 22:34:57 -0500

    test: add @octocore mock
    Don't actually neeed this here with my upcoming commit, but just want
    to illustrate that the change is mostly red.
    
    Actually kind of a mystery where this patch disappeared off to...

--- src/utils/github.com/validate-token.ts
+++ src/utils/github.com/validate-token.ts
@@ -1,5 +1,16 @@
 import { Octokit } from '@octokit/core'
 
+jest.mock('@octokit/core', () => ({
+    Octokit: jest.fn().mockImplementation((params: any) => {
+        if (params.auth == 'TOKEN_BAD') {
+            throw new Error('Bad credentials - https://docs.github.com/rest')
+        }
+ 
+        return {
+            request: jest.fn().mockReturnValue({ status: 200 })
+        }
+    })
+}))
 
 export const validateToken = async (data: { token: string }): Promise<boolean> => {
     const headers = { 'X-GitHub-Api-Version': '2022-11-28' }
