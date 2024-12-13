c94ed5bc8d00905c2abc502a7c2b365e1e830b27
JM <jmjanzen@gmail.com>
2024-12-12 22:38:44 -0500

    test: combine octokit mocks
    Nice and DRY. 😎

---
+++ __mocks__/@octokit/core.ts
@@ -0,0 +1,30 @@
+export const Octokit = jest.fn().mockImplementation((params: any) => {
+    if (params.auth == 'TOKEN_BAD') {
+        throw new Error('Bad credentials - https://docs.github.com/rest')
+    }
+
+    return {
+        request: jest.fn().mockImplementation((uri: any) => {
+            // For pulling down readme details
+            if (uri == 'GET /repos/DUCK/QUACK/readme') {
+                return {
+                    data: {
+                        path: 'README.md',
+                        sha: 'good_sha',
+                        content: 'SSBtYXkgaGF2ZSBnb25lIG92ZXJib' +
+                            '2FyZCB3aXRoIHRoZSBkdWNrcyBsb2w='
+                    },
+                }
+            } else if (uri == 'GET /repos/GOOSE/HONK/readme') {
+                throw new Error('Failed to get readme data for GOOSE/HONK')
+            }
+
+            // For pushing updated readme back up
+            if (uri == 'PUT /repos/DUCK/QUACK/contents/README.md') {
+                return { success: 200 }
+            }
+
+            throw new Error('Oops! This uri has not been mocked: ' + uri)
+        })
+    }
+})
--- src/utils/strategies/strategies.test.ts
+++ src/utils/strategies/strategies.test.ts
@@ -1,39 +1,6 @@
 import { getStrategy, execStrategy } from './strategies'
 
 
-jest.mock('@octokit/core', () => ({
-    Octokit: jest.fn().mockImplementation((params: any) => {
-        if (params.auth == 'TOKEN_BAD') {
-            throw new Error('Bad credentials - https://docs.github.com/rest')
-        }
-
-        return {
-            request: jest.fn().mockImplementation((uri: any) => {
-                // For pulling down readme details
-                if (uri == 'GET /repos/DUCK/QUACK/readme') {
-                    return {
-                        data: {
-                            path: 'README.md',
-                            sha: 'good_sha',
-                            content: 'SSBtYXkgaGF2ZSBnb25lIG92ZXJib' +
-                                '2FyZCB3aXRoIHRoZSBkdWNrcyBsb2w='
-                        },
-                    }
-                } else if (uri == 'GET /repos/GOOSE/HONK/readme') {
-                    throw new Error('Failed to get readme data for GOOSE/HONK')
-                }
-
-                // For pushing updated readme back up
-                if (uri == 'PUT /repos/DUCK/QUACK/contents/README.md') {
-                    return { success: 200 }
-                }
-
-                throw new Error('Oops! This uri has not been mocked: ' + uri)
-            })
-        }
-    })
-}))
-
 describe('utils/strategies', () => {
     it('fail to get strategy (unsupported resource/host)', () => {
         expect(() => getStrategy('https://goosehub.honk/waddle/honk'))
